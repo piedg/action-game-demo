748f9f70d0095d2052afdc7987382500d8dedb4f
Pietro <pietro.digiovanni.dev@gmail.com>
2023-03-28 23:29:38 +0200

    script changes

--- Source/ActionGame/Private/AGCharacter.cpp
+++ Source/ActionGame/Private/AGCharacter.cpp
@@ -2,33 +2,94 @@
 
 
 #include "AGCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
-// Sets default values
 AAGCharacter::AAGCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 720.f, 0.f);
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("CameraBoom");
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 450.f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>("ViewCamera");
+	ViewCamera->SetupAttachment(CameraBoom);
 
 }
 
-// Called when the game starts or when spawned
 void AAGCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	//GetMesh()->HideBoneByName(TEXT("weapon"), EPhysBodyOp::PBO_None);
+	//GetMesh()->HideBoneByName(TEXT("pistol"), EPhysBodyOp::PBO_None);
 }
 
-// Called every frame
 void AAGCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
-// Called to bind functionality to input
 void AAGCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &AAGCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AAGCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &AAGCharacter::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &AAGCharacter::LookUp);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Aim", IE_Pressed, this, &ACharacter::Jump);
 }
 
+void AAGCharacter::MoveForward(float Value)
+{
+	if (Controller && (Value != 0.f))
+	{
+		// find out which way is forward
+		const FRotator ControlRotation = GetControlRotation();
+		const FRotator YawRotation(0.f, ControlRotation.Yaw, 0.f);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AAGCharacter::MoveRight(float Value)
+{
+	if (Controller && (Value != 0.f))
+	{
+		// find out which way is right
+		const FRotator ControlRotation = GetControlRotation();
+		const FRotator YawRotation(0.f, ControlRotation.Yaw, 0.f);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AAGCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void AAGCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
+void AAGCharacter::Aim()
+{
+	UE_LOG(LogTemp, Display, TEXT("Aiming..."));
+	IsAiming = true;
+}
--- Source/ActionGame/Public/AGCharacter.h
+++ Source/ActionGame/Public/AGCharacter.h
@@ -6,27 +6,34 @@
 #include "GameFramework/Character.h"
 #include "AGCharacter.generated.h"
 
+
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class ACTIONGAME_API AAGCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AAGCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+	virtual void BeginPlay() override;
+
 	void MoveForward(float Value);
 	void MoveRight(float Value);
 	void Turn(float Value);
 	void LookUp(float Value);
+	void Aim();
+
+	UPROPERTY(VisibleAnywhere)
+		USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere)
+		UCameraComponent* ViewCamera;
+
+	bool IsAiming = false;
 };
